5a94cc0dbc47732b940620d2be6f7e5ef6caa85d
Ludovic Dupre <dupreludovic1@gmail.com>
2023-04-11 16:14:29 +0200

    Dependency injection commit
    Stuck at "cartService" business particularities?

--- test_project/src/app/app.module.ts
+++ test_project/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainingsComponent } from './components/trainings/trainings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
