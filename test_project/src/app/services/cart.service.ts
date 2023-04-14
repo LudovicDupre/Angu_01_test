db811c5a7406fe3d7b717e464cc8b884d0177c42
Ludovic Dupre <dupreludovic1@gmail.com>
2023-04-14 15:09:52 +0200

    Update cart.service.ts

--- test_project/src/app/services/cart.service.ts
+++ test_project/src/app/services/cart.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Training } from '../models/training.model';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  
-  cart : Training[]= [];
-  
-  addTraining(product :Training) {
+
+  cart: Training[] = JSON.parse(localStorage.getItem("cart") || '[]');
+
+  addTraining(product: Training): Training[] {
     this.cart.push(product);
+    localStorage.setItem('', JSON.stringify(this.cart));
+    return this.cart;
+  }
+
+  scanCart() {
+    this.cart.forEach((p) => {
+     // if(p.getQuantity() ==0) this.cart.delete(p.getId()));
+    })
   }
 
   getCart() {
@@ -18,6 +27,7 @@ export class CartService {
 
   clearCart() {
     this.cart = [];
+    localStorage.setItem('cart', JSON.stringify([]));
     return this.cart;
   }
 
